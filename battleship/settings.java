752ecf450aab7691b4e721056282cbb02406700f
Maxpols <71509710+Maxpols@users.noreply.github.com>
2020-10-26 23:13:25 +0100

    Add files via upload

---
+++ battleship/settings.java
@@ -0,0 +1,12 @@
+package battleship;
+
+//some modifiers and variables I want to be able to set the access modifiers for bc I just learnt how to do that
+public class settings {
+
+    final static int fieldSize = 12;
+    public int getFieldSize() {
+        return fieldSize;
+    }
+
+    static int turn = -1;
+}
